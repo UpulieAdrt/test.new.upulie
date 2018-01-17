8d712be5a160b9c7b5cddf59d2b3fe6e55ada524
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-17 14:32:03 +0530

    API trigger added

--- test.new.upulie/test.new.upulie-lambda.js
+++ test.new.upulie/test.new.upulie-lambda.js
@@ -2,7 +2,7 @@ let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
 
-	callback(null,'Successfully executed _ :)');
+	callback(null,'Successfully executed');
 	console.log("Success");
-	console.log("failed" ,Error)
+
 }
